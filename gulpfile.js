af175e458632d74c92f3bf3264104ddc02ee41b1
Vidula Bhandarkar <vidulamb@gmail.com>
2016-11-27 09:40:53 -0800

    Add gulpfile.js via upload
    gulpfile.js edited to incorporate following changes:
    1. moved code for minification of main.js under 'scripts' task
    2. created a default task list for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,38 +7,41 @@ var cleanCSS = require('gulp-clean-css');
 var browserSync = require('browser-sync').create(); // 
 var reload = browserSync.reload;
 
-gulp.task('default', function() {
+gulp.task('scripts', function() {
   // place code for your default task here
-  return gulp.src('./js/main.js')
+  return gulp.src('./src/js/main.js')
     // This will output the non-minified version
-    .pipe(gulp.dest('./js'))
+    .pipe(gulp.dest('./src/js'))
     // This will minify and rename to main.min.js
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('./js'));
+    .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('minify-css', function() {
   // Get source file for css
-  return gulp.src('./css/main.css')
+  return gulp.src('./src/css/main.css')
     // Output the non-minified version of the css file
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./src/css'))
     // Minify the css file, rename to main.min.css, and save under ./css folder
     .pipe(cleanCSS())
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('browser-sync', function() {
    browserSync.init({
      server: {
        port: 'localhost:8888',
-       baseDir: "./",
+       baseDir: "./dist/",
        index: "index.html"
      }
    });
 
-   gulp.watch("*.html").on("change", reload);
-   gulp.watch("./js/*.js").on("change", reload);
-   gulp.watch("./css/*.css").on("change", reload);
+   gulp.watch("./src/*.html").on("change", reload);
+   gulp.watch("./src/js/*.js").on("change", reload);
+   gulp.watch("./src/css/*.css").on("change", reload);
 });
+
+// default gulp task
+gulp.task('default', ['minify-css', 'scripts']);
